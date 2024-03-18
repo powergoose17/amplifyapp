3331a6caf3f45a7f19671cbe3f2436140b7771c0
bridge-nacba <bridge.nacba@gmail.com>
2024-03-18 17:40:41 -0400

    just to see amplifyat work

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React V2 to see amplify at work
         </a>
       </header>
     </div>
